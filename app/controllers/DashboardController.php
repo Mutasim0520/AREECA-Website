19f2fc400d39b569b5f34e7d888fc5cd57dd34ec
Mutasim0520 <mutasimfuad0520@gmail.com>
2024-08-24 15:42:00 +0200

    created dashboard controller

---
+++ app/controllers/DashboardController.php
@@ -0,0 +1,14 @@
+<?php
+
+class DashboardController extends Controller {
+
+    public function dashboard() {
+        $userModel = $this->model('User'); // Loads the User model
+        $user = $userModel->getUserById(1);
+        // $this->view('home/index', ['user' => $user]); // Passing data to the view
+        // $user = $this->model('User');
+        // $userData = $user->getUser(1);
+        // $this->view('home/index', ['name' => $userData['name']]);
+        $this->view('index', []);
+    }
+}
